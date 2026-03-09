47a60f2d88126ab5086bdc26b0f1c41d1533e39e
C0ldCl0ud <lang.m.lang.m@googlemail.com>
2026-03-09 08:51:36 +0100

    Document data processing steps in main.py
    Added comments outlining the data processing steps and questions regarding encryption and differential privacy.

--- src/main.py
+++ src/main.py
@@ -1,1 +1,11 @@
+"""
+1) load data
+2) preprocess data? encrypted?
+3) encrypt data
+4) test/train split
+5) mpc traing
+6) calculate mpc roc for thresholds
+7) plot auc
 
+where does dp fit in?
+"""
