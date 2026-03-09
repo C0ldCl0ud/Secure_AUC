82262f4c6d639de23a44583ced53cdd2322ed663
Brauckhoff <Tim.Brauckhoff@b-tu.de>
2026-03-09 11:14:41 +0100

    mpc encrypt method

--- src/mpc.py
+++ src/mpc.py
@@ -1,1 +1,18 @@
+import crypten
+import torch
 
+
+def encrypt(vector, parties):
+
+    # transform
+    values = vector.iloc[:, 0].tolist()
+
+    @crypten.mpc.run_multiprocess(world_size=parties)
+    def encr(values):
+        x = torch.tensor(vector)
+        x_enc = crypten.cryptensor(x)
+        return x_enc
+
+    res = encr(vector)
+
+    return res
