2f7cac88179adc99f8e1423df82b29a851d9a505
Selina-Wint <selina.winter@student.uni-tuebingen.de>
2026-03-10 10:52:53 +0100

    update

--- src/dp.py
+++ src/dp.py
@@ -1,1 +1,15 @@
+import numpy as np
+import auc_analysis
+import mpc
 
+def differential_auc():
+    sensitivity = 1
+
+    epsilon = 0.1
+    scale = sensitivity / epsilon
+
+    noise = np.random.laplace(loc=0, scale=scale)
+
+    noisy_auc_score_scikit =  auc_analysis.auc + noise
+
+    noisy_auc_score = mpc. + noise
